917fe9876a4bf3da40e298c1d49c99f32866f78b
Ethan D <erdesi90562@gmail.com>
2016-01-26 07:42:10 -0500

    Assigned string to variable

---
+++ variables.js
@@ -0,0 +1,4 @@
+var example = 'some string'; // ES5 var. ES6 let or conts
+// let example = 'some string'; // ES6
+// const example = 'some string'; // ES6
+console.log(example); // print 'some string' to console
